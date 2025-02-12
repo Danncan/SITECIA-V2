3f03f452cb9356a5a0e005da6e64cc20a0d82722
Danncan <dannyse2004@gmail.com>
2025-02-12 11:49:23 -0500

    Pruebas

--- js/Proyectos.js
+++ js/Proyectos.js
@@ -1,25 +1,4 @@
 
-        function reloadCSS() {
-          const links = document.querySelectorAll('link[rel="stylesheet"]');
-          links.forEach(link => {
-            const href = link.getAttribute('href');
-            link.setAttribute('href', href + '?v=' + new Date().getTime());
-          });
-        }
-      
-        function reloadJS() {
-          const scripts = document.querySelectorAll('script[src]');
-          scripts.forEach(script => {
-            const src = script.getAttribute('src');
-            script.setAttribute('src', src + '?v=' + new Date().getTime());
-          });
-        }
-      
-        document.addEventListener("DOMContentLoaded", () => {
-          reloadCSS();
-          reloadJS();
-        });
-
         let swiperInstance;
 
         function initializeSwiper() {
